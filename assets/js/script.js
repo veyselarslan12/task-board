998cd1e20af87fe1d23474a4afc716eb51fd00de
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-04-04 01:23:59 -0500

    created new task card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,92 @@
 // Retrieve tasks and nextId from localStorage
-// let taskList = JSON.parse(localStorage.getItem("tasks"));
+let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
-const projectFormEl = $('.btn-custom')
-const projectNameInputEl = $('#task-title-input')
-const projectTypeInputEl = $('#text-description')
-const projectDateInputEl = $('#datepicker')
+const projectFormEl = $(".btn-custom");
+const projectNameInputEl = $("#task-title-input");
+const projectTypeInputEl = $("#text-description");
+const projectDateInputEl = $("#datepicker");
 
+const todoListEl = $('#todo-cards')
+const inProgressListEl = $('#in-progress-cards')
+const doneListEl = $('#done-cards')
 
-
-$('#datepicker').datepicker()
-
+$("#datepicker").datepicker();
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
-
+    
 
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-
+    const newCard = $(`
+        <div class="card" data-id="${task.id}" data-status="${task.status}"> 
+            <div class="card-body">
+                <h5 class="card-title">${task.name}</h5>
+                <p class="card-text">${task.type}</p>
+                <p class="card-text">${task.dateDue}</p>
+                <button class="btn btn-danger">Delete</button>
+            </div>
+        </div>
+    `)
+    return newCard
 
 }
 
+
+
+
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
-
+   const savedProjects = loadProjectFromLocalStorage()
+    for (const task of savedProjects) {
+        const cardEl = createTaskCard(task)
+    }
 }
 
 // Todo: create a function to handle adding a new task
 function loadProjectFromLocalStorage() {
-    let taskList = JSON.parse(localStorage.getItem('projects')) || [];
-    return taskList;
+  let taskList = JSON.parse(localStorage.getItem("projects")) || [];
+  return taskList;
 }
 
 function saveProjectToLocalStorage(tasks) {
-    localStorage.setItem('projects', JSON.stringify(tasks))
+  localStorage.setItem("projects", JSON.stringify(tasks));
 }
-function handleAddTask(event){
-    event.preventDefault();
+function handleAddTask(event) {
+  event.preventDefault();
 
-    const title =  projectNameInputEl.val()
-    const description = projectTypeInputEl.val()
-    const dueDate = projectDateInputEl.val()
-    
-    projectNameInputEl.val('')
-    projectTypeInputEl.val('')
-    projectDateInputEl.val('')
+  const title = projectNameInputEl.val();
+  const description = projectTypeInputEl.val();
+  const dueDate = projectDateInputEl.val();
 
-    let taskList = loadProjectFromLocalStorage()
+  let taskList = loadProjectFromLocalStorage();
 
-    const newProject = {
-        name: title,
-        type: description,
-        dateDue: dueDate,
-    }    
-    
-    taskList.push(newProject);
+  const newProject = {
+    id: Math.random(),
+    name: title,
+    type: description,
+    dateDue: dueDate,
+    status: 'todo'
+  };
 
-    saveProjectToLocalStorage(taskList);
+  taskList.push(newProject);
+
+  saveProjectToLocalStorage(taskList);
+
+  renderTaskList()
 
 
-    
+  projectNameInputEl.val("");
+  projectTypeInputEl.val("");
+  projectDateInputEl.val("");
 }
-projectFormEl.on('click', handleAddTask)
+projectFormEl.on("click", handleAddTask);
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+function handleDeleteTask(event) {
+
 
 
 }
@@ -77,10 +95,13 @@ function handleDeleteTask(event){
 function handleDrop(event, ui) {
 
 
+
+
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+
 
 
 });
